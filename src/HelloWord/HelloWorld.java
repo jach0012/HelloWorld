7a515faf2bb7a29414a519656e70f6eae2113db4
JKY <jacky@bbs-suew.de>
2019-05-25 10:45:29 +0200

    Nein. Ohne 2!

--- src/HelloWord/HelloWorld.java
+++ src/HelloWord/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-        // Ausgabe Hello World!
+        // Ausgabe Hello World! ohne 2!
         System.out.println("Hello World!");
 
 	}
