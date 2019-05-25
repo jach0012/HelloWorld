8406b67b152a35e1226136fc7b151f418b14774e
JKY <jacky@bbs-suew.de>
2019-05-25 10:21:02 +0200

    Hi folks

---
+++ src/HelloWord/HelloWorld.java
@@ -0,0 +1,12 @@
+package HelloWord;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // Ausgabe Hello World!
+        System.out.println("Hello World!");
+
+	}
+
+}
